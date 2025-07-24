f43935a6959edc2f81f07ad55c93de06ba6def0e
OsayedAlazzam <osayedalazzam@gmail.com>
2025-07-24 13:48:25 +0300

    Creat Pyramid Star Patterns Using JavaScript

---
+++ Pyramid Star Patterns Using JavaScript.js
@@ -0,0 +1,13 @@
+
+function PyramidStarPattern(){
+    let MaxStars=7;
+
+    for(let i = 1; i <= MaxStars; i++){
+        console.log("*".repeat(i));
+    }
+    for(let i = MaxStars-1; i >= 1; i--){
+        console.log("*".repeat(i));
+    }
+}
+
+PyramidStarPattern();
